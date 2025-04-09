ee3604252f4d1d4b887857fe3312f07f4130111b
root <root@dev-vm.34nsu5jr5cguvnba05aeg14zeb.ux.internal.cloudapp.net>
2025-04-09 19:46:48 +0000

    added

---
+++ demo-web-app/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello, %s!", name);
+    }
+    
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Demo Web Application!";
+    }
+}
